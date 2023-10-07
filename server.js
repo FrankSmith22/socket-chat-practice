1ae570aea39f5d763e0e6a19ca2d98b590aa448a
Frank Smith <fasvi6@gmail.com>
2023-10-07 16:13:18 -0400

    Disconnect message appears on disconnect, and messages are labelled with a name

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const io = require('socket.io')(3000)
 
+const users = {}
+
 io.on('connection', socket => {
-    console.log('Hello world')
-    socket.emit('chat-message', 'Hello World')
+
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
 
     socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message)
+        socket.broadcast.emit('chat-message', { message, name: users[socket.id] })
+    })
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
     })
 })
